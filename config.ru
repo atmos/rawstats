dbde82ddaac0b5a34bc94f6c27d27d33dbf52b9e
Corey Donohoe <atmos@atmos.org>
2010-10-17 13:22:10 -0700

    simplify a couple of things

--- config.ru
+++ config.ru
@@ -1,9 +1,5 @@
-require 'erb'
 require 'rubygems'
-require 'rack-legacy'
 require 'lib/rawstats'
-
-Rawstats::ConfigFile.setup
 
 use Rack::Static, :urls => ["/css", "/icon", "/js", "/classes"], :root => "public"
 use Rack::Legacy::Cgi, "public"
--- lib/rawstats.rb
+++ lib/rawstats.rb
@@ -1,10 +1,19 @@
+require 'erb'
+require 'fileutils'
+require 'rack-legacy'
+
 module Rawstats
   class ConfigFile
     def self.setup
       new.run
     end
 
+    def self.root
+      File.expand_path(File.join(File.dirname(__FILE__), '..'))
+    end
+
     def run
+      FileUtils.mkdir_p data_directory
       File.open(config_file, "w") do |file|
         rendered_config_file = ERB.new(File.read("#{config_file}.erb"), 0).result(binding)
         file.puts(rendered_config_file)
@@ -12,7 +21,7 @@ module Rawstats
     end
 
     def data_directory
-      ENV['RAWSTATS_DATA_DIR'] || "/home/atmos/rawstats/local-data"
+      ENV['RAWSTATS_DATA_DIR'] || "#{self.class.root}/local-data"
     end
 
     def log_file_command
@@ -24,3 +33,4 @@ module Rawstats
     end
   end
 end
+Rawstats::ConfigFile.setup
